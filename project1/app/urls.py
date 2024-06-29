91612417d1db66e3143fc4bb85b543a707bff763
Himanshi Mate <matemanu725@gmail.com>
2024-06-29 21:33:15 +0530

    import

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 # from app import views
 # from .views import home 
+from .views import home1 
 from .views import register
-
+from .views import registerdata
 urlpatterns = [
     #  path('home/', views.home),
-     path('registration/',register)
+     path('registration/',register),
+     path('home1/',home1),
+     path('register/',register, name='register'),
+     path('registerdata/', registerdata, name='register')
+
     # path('hame',home)
 ]
--- project1/app/views.py
+++ project1/app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,render,redirect
 from django.http import HttpResponse, JsonResponse
+
 # Create your views here.
 def home(request):
     # return HttpResponse("welcome app urls")
@@ -7,3 +8,12 @@ def home(request):
     return JsonResponse(data)
 def register(request):
     return render(request, 'registration.html')
+
+
+def home1(request):
+    return redirect("https://www.amazon.in/")
+
+def registerdata(request):
+    # return
+    print(request.method)
+    print(request.POST)
